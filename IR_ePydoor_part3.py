f8730e373dbfd71c2cbdac7526977af0fed727b0
Juan_Luis_Trincado <juanluis.trincado@upf.edu>
2018-11-28 10:28:58 +0100

    Added IR_ePydoor_part3

--- IR_ePydoor_part3.py
+++ IR_ePydoor_part3.py
@@ -76,83 +76,23 @@ def main():
         # ONLY FOR HYDRA
         #python2 = "Python/2.7.11"
 
-        # readcounts_path = "/projects_rg/SCLC_cohorts/George/PSI_Junction_Clustering/readCounts_George_Peifer_Rudin_Yokota.tab"
-        # # bam_path = "/projects_rg/SCLC_cohorts/George/STAR/George_and_Peifer"
-        # bam_path = "/projects_rg/SCLC_cohorts/Rudin/STAR/Rudin_Yokota"
-        # coverage_path = "/projects_rg/SCLC_cohorts/coverageBed/"
-        # gtf_path = "/projects_rg/SCLC_cohorts/annotation/Homo_sapiens.GRCh37.75.formatted.only_protein_coding.gtf"
-        # max_length = 500
-        # threshold = 5
-        # threshold2 = 10
-        # n_randomizations = 100
-        # mutations_path = "/projects_rg/babita/TCGA/mutation/mut_pipeline/juanlu_sclc/src_files/SCLC_mutations_sorted.bed.mut.out"
-        # repeats_path = "/projects_rg/SCLC_cohorts/cis_analysis/tables/hg19_repeats.bed"
-        # output_path = "/users/genomics/juanluis/SCLC_cohorts/test"
-
-        # 6. Create the folder, if it doesn't exists
-        logger.info("Part6...")
-        if not os.path.exists(output_path + "/coverageBed"):
-            os.makedirs(output_path + "/coverageBed")
-        # Move all the coverage.sorted files to the created directory
-        command1="mv "+output_path+"/*coverage_sorted "+output_path + "/coverageBed/"
-        os.system(command1)
-
-        # 7.1. Get the coverage for each exonization
-        logger.info("Part7...")
-        get_coverageBed_adapter(output_path + "/IR_expressed_genes.tab", output_path + "/random_introns.bed",
-                        output_path + "/coverageBed", output_path, name_user)
-
-        # 7.2. Assemble all pieces into one single file
-        command2="awk 'FNR==1 && NR!=1{next;}{print}' "+output_path+"/get_coverageBed_results.*.tab > "+output_path+"/get_coverageBed_results.tab"
-        os.system(command2)
-
-        # 7.3. Get the introns with a significant p_value
-        command3="head -n1 "+output_path+"/get_coverageBed_results.tab > "+output_path+"/IR_significant_introns.tab; " \
-                   "awk '{ if ($7 <= 0.05) print }' "+output_path+"/get_coverageBed_results.tab >> "+output_path+"/IR_significant_introns.tab"
-        os.system(command3)
-
-        # 8. Get the peptide sequence associated
-        logger.info("Part8...")
-        get_peptide_sequence(output_path + "/IR_significant_introns_TEST.tab", transcript_expression_path, gtf_path, codons_gtf_path,
-                             output_path + "/IR_peptide_sequence.fa", output_path + "/IR_fasta_sequence.fa",
-                             output_path + "/IR_ORF.tab", output_path + "/IR_ORF_sequences.tab", output_path + "/IR_Interpro.tab",
-                             output_path + "/IR_IUPred.tab", mosea, fast_genome, orfs_scripts, interpro,IUPred, remove_temp_files,
-                             python2)
-
-        # 9. Filter the significant results
-        logger.info("Part9...")
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        command4="module load R; Rscript "+dir_path+"/lib/IR/filter_results.R "+output_path + "/IR_ORF.tab"+" "+ \
-                 output_path + "/IR_ORF_filtered.tab" +" "+output_path + "/IR_ORF_filtered_peptide_change.tab"
-        os.system(command4)
-
-        # 10. Select the fasta candidates for being run to the epitope analysis
-        logger.info("Part10...")
-        #Create the folder, if it doesn't exists
-        if not os.path.exists(output_path + "/IR_fasta_files"):
-            os.makedirs(output_path + "/IR_fasta_files")
-        select_fasta_candidates(output_path + "/IR_ORF_filtered_peptide_change.tab", output_path + "/IR_peptide_sequence.fa", output_path + "/IR_peptide_sequence_filtered.fa", output_path + "/IR_fasta_files")
-
-        #11. Run netMHC-4.0_part1
-        logger.info("Part11...")
-        if not os.path.exists(output_path + "/IR_NetMHC-4.0_files"):
-            os.makedirs(output_path + "/IR_NetMHC-4.0_files")
-        run_netMHC_classI_slurm_part1(output_path + "/IR_ORF_filtered_peptide_change.tab", HLAclass_path, HLAtypes_path,
+        #13. Run netMHC-4.0_part2
+        logger.info("Part13...")
+        run_netMHC_classI_slurm_part2(output_path + "/IR_ORF_filtered_peptide_change.tab", HLAclass_path, HLAtypes_path,
                                       output_path + "/IR_fasta_files",output_path + "/IR_NetMHC-4.0_files", output_path + "/IR_NetMHC-4.0_neoantigens_type_3.tab",
                                       output_path + "/IR_NetMHC-4.0_neoantigens_type_3_all.tab", output_path + "/IR_NetMHC-4.0_neoantigens_type_2.tab",
                                       output_path + "/IR_NetMHC-4.0_neoantigens_type_2_all.tab", output_path + "/IR_NetMHC-4.0_junctions_ORF_neoantigens.tab",
                                       netMHC_path)
 
-        #12. Run netMHCpan-4.0_part1
-        logger.info("Part12...")
-        if not os.path.exists(output_path + "/IR_NetMHCpan-4.0_files"):
-            os.makedirs(output_path + "/IR_NetMHCpan-4.0_files")
-        run_netMHCpan_classI_slurm_part1(output_path + "/IR_ORF_filtered_peptide_change.tab", HLAclass_path, HLAtypes_pan_path,
+        #14. Run netMHCpan-4.0_part2
+        logger.info("Part14...")
+        run_netMHCpan_classI_slurm_part2(output_path + "/IR_ORF_filtered_peptide_change.tab", HLAclass_path, HLAtypes_pan_path,
                                       output_path + "/IR_fasta_files",output_path + "/IR_NetMHCpan-4.0_files", output_path + "/IR_NetMHCpan-4.0_neoantigens_type_3.tab",
                                       output_path + "/IR_NetMHCpan-4.0_neoantigens_type_3_all.tab", output_path + "/IR_NetMHCpan-4.0_neoantigens_type_2.tab",
                                       output_path + "/IR_NetMHCpan-4.0_neoantigens_type_2_all.tab", output_path + "/IR_NetMHCpan-4.0_junctions_ORF_neoantigens.tab",
                                       netMHC_pan_path)
-        logger.info("Wait until all jobs have finished. Then, go on with part3")
+
+        logger.info("Done.")
 
         exit(0)
 
